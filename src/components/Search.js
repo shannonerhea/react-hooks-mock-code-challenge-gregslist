28a92b495d491f755023c5dfc0c6ea37f466d1a2
shannone <shannone426@gmail.com>
2022-09-28 11:21:55 -0400

    rendered all listing, added delete added fave toggle

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search({ }) {
+function Search() {
+  const [currentSearch, setCurrentSearch] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
@@ -12,8 +13,8 @@ function Search({ }) {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={ currentSearch }
+        onChange={(e) => setCurrentSearch(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
