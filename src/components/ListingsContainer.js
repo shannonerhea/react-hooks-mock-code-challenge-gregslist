67b53d5fbe6b032b20e0f317b6fff9f174c391f1
shannone <shannone426@gmail.com>
2022-09-28 11:02:55 -0400

    rendered all listing, added delete added fave toggle

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,32 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((res) => res.json())
+    .then(listings => setListings(listings))
+  },[]);
+
+  function handleDelete(id) {
+    const updatedListing = listings.filter(listing => listing.id !== id)
+    setListings(updatedListing)
+  };
+
+  const listingCards = listings.map((listing) => {
+    return <ListingCard 
+    key={ listing.id } 
+    listing={ listing }
+    onDelete={ handleDelete }
+    />;
+  })
+
+
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <ul className="cards">{ listingCards }</ul>
     </main>
   );
 }
