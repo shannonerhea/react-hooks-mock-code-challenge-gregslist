50ef5e815f8d8bc1bde65c1195a1ee8c6611d0d1
shannone <shannone426@gmail.com>
2022-10-02 08:50:20 -0400

    added diagram

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.production.min";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch(" http://localhost:6001/listings")
+    .then((r) => r.json())
+    .then(listingArr => {
+      setListings(listingArr);
+    })
+  },[]);
+
+  
   return (
     <div className="app">
       <Header />
